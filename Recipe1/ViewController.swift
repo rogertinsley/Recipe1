3bb9a6968d759d7d67c734593bea87f008246aa0
Roger Tinsley <mail@rogertinsley.co.uk>
2015-06-03 21:55:54 +0100

    Initial commit

--- Recipe1/ViewController.swift
+++ Recipe1/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+    let array = ["item1", "item2", "item3"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("customcell") as! UITableViewCell
+        cell.textLabel?.text = array[indexPath.item]
+        
+        return cell;
+    }
 
 }
 
