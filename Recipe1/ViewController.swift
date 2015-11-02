0f5bd04454f5b97d5f939271f7211878e47813c0
Roger Tinsley <rogertinsley@Rogers-MBP.home>
2015-11-02 20:58:55 +0000

    Added background images

--- Recipe1/ViewController.swift
+++ Recipe1/ViewController.swift
@@ -15,11 +15,19 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.rowHeight = 70
+        tableView.backgroundView = UIImageView(image: UIImage(named: "breads"))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.navigationBar.alpha = 0.5
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -28,6 +36,15 @@ class ViewController: UITableViewController {
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("customcell", forIndexPath: indexPath) as UITableViewCell
+        
+        if (indexPath.item % 2 == 0) {
+            cell.backgroundColor = UIColor.clearColor()
+        } else {
+            cell.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.2)
+            cell.textLabel?.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.0)
+        }
+        
+        cell.textLabel?.textColor = UIColor.whiteColor()
         cell.textLabel?.text = array[indexPath.item]
         
         return cell;
